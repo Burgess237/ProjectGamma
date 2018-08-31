eb361c300314b9233d79f9ccd8790a8a04211eab
Burgess237 <39771407+Burgess237@users.noreply.github.com>
2018-08-31 14:46:03 +0200

    Update row-generator.component.ts

--- src/app/row-generator/row-generator.component.ts
+++ src/app/row-generator/row-generator.component.ts
@@ -18,28 +18,33 @@ num2: any;
 
 ColArr: any[] = [];
 RowArr: any[] = [];
-RowStart = '<div class = "row">';
+/* RowStart = '<div class = "row">';
 Column = '<div class = "col">';
+content = '<app-content></app-content>';
 RowEnd = '</div>';
-GeneratedRows = this.createGrid();
+GeneratedRows = this.createGrid(); */ 
+
+//this generated the rows
 createGrid() {
   this.num1 = Math.round((Math.random() * 2) + 2);
   this.num2 = Math.round((Math.random() * 4) + 4);
   for ( this.i = 1; this.i <= this.num1; this.i++) {
-    this.html += this.RowStart;
+    // this.html += this.RowStart;
       for ( this.j = 1; this.j <= this.num2; this.j++) {
         this.ColArr.push(this.j);
-        this.html += this.Column;
+      //  this.html += this.Column;
+      //  this.html += this.content;
         // method to generate random stuff?
     }
     this.RowArr.push(this.i);
-    this.html += this.RowEnd;
+    // this.html += this.RowEnd;
   }
-  return this.html;
+    // return this.html;
 }
 constructor(private sanitizer: DomSanitizer) { }
   ngOnInit() {
-    this.htmlData = this.sanitizer.bypassSecurityTrustHtml(this.GeneratedRows);
+    this.createGrid();
+    // this.htmlData = this.sanitizer.bypassSecurityTrustHtml(this.GeneratedRows);
   }
 
 }
